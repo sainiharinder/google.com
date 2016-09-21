1db2ff50de14ac2d159e83cf23700cce0e8df051
sainiharinder <harindersaini@live.in>
2016-09-20 23:08:08 -0700

    DZD
    ZD

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
 console.log('This would be the main JS file.');
+function setup(){
+	loadJSon("http://api.open-notify.org/astros.json", gotData, 'jsonp');
+}
+function gotData(data){
+	println(data);
+}
+  
